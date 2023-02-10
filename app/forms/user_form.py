f0de439dda82b73ec8d0e64cb8de6f48a3678824
Ryan Harden <ryanharden20@gmail.com>
2023-02-10 15:32:10 -0800

    forms done

---
+++ app/forms/user_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+class UserForm(FlaskForm):
+    email=StringField('Email', validators=[DataRequired()])
+    display_name=StringField('Display Name')
+    first_name=StringField('First Name')
+    last_name=StringField('Last Name')
+    city=StringField('City')
+    country=StringField('Country')
+    bio=StringField('Bio', validators=[Length(min=0, max=255, message='Max 255 characters')])
+    profile_image_url=StringField('Profile Image')
+    header_image_url=StringField('Display Image')
