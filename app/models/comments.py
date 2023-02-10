7de9f57314f91069b83e050b4d957eb6fb80e436
Ryan Harden <ryanharden20@gmail.com>
2023-02-10 14:21:31 -0800

    models update

--- app/models/comments.py
+++ app/models/comments.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+import datetime
 
 class Comments(db.Model):
     __tablename__="comments"\
@@ -11,4 +12,4 @@ class Comments(db.Model):
     user_id = db.Column(db.Integer, db.ForeinKey(add_prefix_for_prod("users.id")), nullable=False)
     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("songs.id")), nullable=False)
     time = db.Column(db.Decimal, nullable=False)
-    created_at = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
