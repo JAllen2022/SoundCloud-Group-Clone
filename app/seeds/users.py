a0358b347acf4c30ea07741497ae9ec60e99da75
Ryan Harden <ryanharden20@gmail.com>
2023-02-10 16:26:02 -0800

    seeds done

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,18 +1,25 @@
 from app.models import db, User, environment, SCHEMA
 
 
+demo = User(
+    username='Demo', email='demo@aa.io', password='password')
+roysa = User(
+    username='roysa', email='roysa@aa.io', password='password')
+jason = User(
+    username='jason', email='jason@aa.io', password='password')
+ryan = User(
+    username="ryan", email="ryan@aa.io", password="password")
+lillyann = User(
+    username="lillyann", email="lillyann@aa.io", password="password")
+
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-
+    
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(roysa)
+    db.session.add(jason)
+    db.session.add(ryan)
+    db.session.add(lillyann)
     db.session.commit()
 
 
@@ -27,5 +34,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute("DELETE FROM users")
-        
+
     db.session.commit()
