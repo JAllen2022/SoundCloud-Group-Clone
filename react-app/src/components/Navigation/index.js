9d0ee6d84b2a7f8983fa8129d62454c39d03d0d0
lilly <lcrhidalgo99@gmail.com>
2023-02-13 15:39:15 -0500

    progress: song / css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,31 +1,43 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
 // import UploadPage from './Upload/UploadPage';
-import './Navigation.css';
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<div className='nav-page-container'>
-			<div className='nav-bar'>
-
-			<NavLink exact to="/">Home</NavLink>
-
-			{isLoaded && (
-				<li>
-					{/* <NavLink exact to="/">Home</NavLink> */}
-					<NavLink exact to="/upload">Upload</NavLink>
-					<ProfileButton user={sessionUser} />
-
-				</li>
-
-			)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="nav-page-container">
+            <div className="nav-bar">
+                <div className="header-left">
+                    <NavLink exact to="/">
+                        Home
+                    </NavLink>
+                </div>
+                <div className="header-right">
+                    {isLoaded && (
+                        <li>
+                            {/* <NavLink exact to="/">Home</NavLink> */}
+                            <div className="upload-button-container">
+                                <NavLink
+                                    exact
+                                    to="/upload"
+                                    className="upload-button"
+                                >
+                                    Upload
+                                </NavLink>
+                            </div>
+                            <div className="profile-button-container">
+                                <ProfileButton user={sessionUser} />
+                            </div>
+                        </li>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Navigation;
