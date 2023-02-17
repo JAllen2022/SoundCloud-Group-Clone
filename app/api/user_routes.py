8d36af28f5426b2d93860ae62312c96a57a3791d
JAllen2022 <jasonallen715@gmail.com>
2023-02-16 21:42:09 -0800

    Finished validations for the user edit page

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -24,6 +24,10 @@ def user(id):
     Query for a user by userId and returns that user in a dictionary
     """
     user = User.query.get(id)
+
+    if not user:
+        return {"errors":"User not found"}, 404
+
     return user.to_dict()
 
 
@@ -36,10 +40,11 @@ def get_user_comments(id):
     """
     user = User.query.get(id)
 
-    # if user.comments:
+    if not user:
+        return {"errors":"User not found"}, 404
+
     return {comment.id:comment.to_dict() for comment in user.comments}
-    # else:
-    #     return {"Error": "No Comments Found"}
+
 
 
 @user_routes.route("/<int:id>/likes")
@@ -48,6 +53,10 @@ def get_user_likes(id):
       Query for all liked songs by userId
     """
     user = User.query.get(id)
+
+    if not user:
+        return {"errors":"User not found"}, 404
+
     songs = user.user_likes
     return {song.id:song.to_dict_single_song() for song in songs}
 
@@ -58,6 +67,10 @@ def get_user_songs(id):
       Query for all songs by userId
     """
     user = User.query.get(id)
+
+    if not user:
+        return {"errors":"User not found"}, 404
+
     songs = user.songs
     return {song.id:song.to_dict_single_song() for song in songs}
 
@@ -70,30 +83,33 @@ def edit_user(id):
       Route to edit the user profile
     """
     user = User.query.get(id)
+
     if not user:
-        return {"Error": "User not found"}, 404
+        return {"errors": "User not found"}, 404
 
     profile_image_url = user.profile_image_url
 
     if "profile_picture" in request.files:
+        # Next, upload the image to AWS
+        profile_picture = request.files["profile_picture"]
+
+        if not s3.image_file(profile_picture.filename):
+            print("errrorrr!!!!!!!!!!!!!! ")
+            return {"errors": "File type not permitted"}, 400
+
         # We need to first delete the existing image in the database for the song
         if profile_image_url:
             imageKey = user.profile_image_url.rsplit('/')[-1]
             s3.remove_image_file_from_s3(imageKey)
 
-        # Next, upload the image to AWS
-        profile_picture = request.files["profile_picture"]
-
-        if not s3.image_file(profile_picture.filename):
-            print("file type not permitted")
-            return {"errors": "file type not permitted"}, 400
 
         profile_picture.filename = s3.get_unique_filename(profile_picture.filename)
 
         upload_image = s3.upload_image_file_to_s3(profile_picture)
 
         if "url" not in upload_image:
-            print("error upload")
+            print("errrorrr!!!!!!!!!!!!!! 212312312 ")
+
             # if the dictionary doesn't have a url key
             # it means that there was an error when we tried to upload
             # so we send back that error message
@@ -126,22 +142,23 @@ def set_header(id):
     user = User.query.get(id)
 
     if not user:
-        return {"Error": "User not found"}, 404
+        return {"errors": "User not found"}, 404
 
     header_image_url = user.header_image_url
 
     if "header_picture" in request.files:
-        # We need to first delete the existing image in the database for the song
-        if header_image_url:
-            imageKey = user.header_image_url.rsplit('/')[-1]
-            s3.remove_image_file_from_s3(imageKey)
-
         # Next, upload the image to AWS
         header_picture = request.files["header_picture"]
 
         if not s3.image_file(header_picture.filename):
             print("file type not permitted")
-            return {"errors": "file type not permitted"}, 400
+            return {"errors": "File type not permitted"}, 400
+
+        # We need to first delete the existing image in the database for the song
+        if header_image_url:
+            imageKey = user.header_image_url.rsplit('/')[-1]
+            s3.remove_image_file_from_s3(imageKey)
+
 
         header_picture.filename = s3.get_unique_filename(header_picture.filename)
 
