a997ef0d0b74dadc6fe173cca116bb7655375e65
Ryan Harden <ryanharden20@gmail.com>
2023-02-16 20:27:33 -0800

    allsongLikes

--- react-app/src/components/Songs/AllSongLikes/AllSongLikes.js
+++ react-app/src/components/Songs/AllSongLikes/AllSongLikes.js
@@ -4,22 +4,74 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSongThunk } from "../../../store/songs";
 
 const AllSongLikes = () => {
-  const dispatch = useDispatch();
-  const { songId } = useParams();
-  const songLikes = useSelector((state) => state.Songs.singleSong.song_likes);
+    const dispatch = useDispatch();
+    const { songId } = useParams();
+    const songLikes = useSelector((state) => state.Songs.singleSong.song_likes);
+    const song = useSelector(state => state.Songs.singleSong)
     console.log("songLikes: ", songLikes)
-  useEffect(() => {
-    dispatch(getSongThunk(songId));
-  }, [dispatch, songId]);
+    useEffect(() => {
+        dispatch(getSongThunk(songId));
+    }, [dispatch, songId]);
 
-  if (!Object.values(songLikes).length) return null;
+    let songLikeItems;
+    if (Object.values(songLikes).length) {
+        const songLikesArr = Object.values(songLikes);
+        songLikeItems = songLikesArr.map((user) => (
+            <div className="song-likes-user-prof-container">
+                <div className="user-pic">
+                    <img alt src={user?.profile_image_url}/>
+                </div>
+                <div className="user-display-name">
+                    {user?.display_name}
+                </div>
+            </div>
+        ))
+    }
+    if (!Object.values(songLikes).length) return null;
+    // asl = all song likes
+    return (
+        <div className="asl-page">
+            <div className="asl-header">
+                <div className="asl-image-container">
+                    <img className="asl-song-image" src={song?.song_image_url} alt={song?.title} />
+                </div>
+                <div className="asl-title-container">
+                  <h1 className="asl-title">{song?.title}</h1>
+                </div>
+            </div>
+            <div className="asl-body">
+                {songLikeItems}
+            </div>
 
-  return (
-    <div>
-      hi
-    </div>
-  )
+        </div>
+    )
 
 }
 
 export default AllSongLikes;
+
+// const UserProfilePic = () => {
+//     const dispatch = useDispatch();
+//     const { songId } = useParams();
+//     const songLikes = useSelector((state) => state.Songs.singleSong.song_likes);
+//     console.log("songLikes: ", songLikes)
+//     useEffect(() => {
+//         dispatch(getSongThunk(songId));
+//     }, [dispatch, songId]);
+
+//     let songLikeItems;
+//     if (Object.values(songLikes).length) {
+//         const songLikesArr = Object.values(songLikes);
+//         songLikeItems = songLikesArr.map((user) => (
+//             <div className="song-likes-user-prof-container">
+//                 <div className="user-pic">
+//                     {user?.profile_image_url}
+//                 </div>
+//                 <div className="user-display-name">
+//                     {user?.display_name}
+//                 </div>
+//             </div>
+
+//         ))
+//     }
+// }
